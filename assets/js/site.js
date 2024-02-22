45ff3544e09fc057dd5528a2ea82835e2d98b170
ZajdelRecipes <160426132+ZajdelRecipes@users.noreply.github.com>
2024-02-22 17:22:45 -0500

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,83 @@
+var recipeList = 
+[
+{"title":"Stuffed Peppers","url":"/stuffed-peppers.html"},
+{"title":"Baked Ziti","url":"/baked-ziti.html"}
+]
+
+var recipes = [];
+
+function search() {
+  var input = document.getElementById("searchText").value;
+	var tokens = input.split(" ");
+	input = "";
+
+  var stopWords = ["and", "or", "i", "the", "of", "a", "as", "at", "is", "are", "to", "if"];
+
+	for(var x=0; x<tokens.length; x++) {
+		var word = tokens[x].toLowerCase();
+
+		for (var y=0; y<stopWords.length; y++) {
+			if(tokens[x] == stopWords[y]) {
+				word = "";
+				break;
+			}
+		}
+
+		if(word != "") {
+			input += tokens[x] + "|";
+		}
+	}
+
+	var cleanedInput = input.substring(0, input.length-1);
+
+	recipes = [];
+
+	for(var i=0; i<recipeList.length; i++) {
+		var recipe = recipeList[i];
+		var reg = new RegExp(cleanedInput, 'gi');
+
+		if(reg.test(recipe.title)) {
+			recipes.push(recipe);
+		}
+	}
+
+	document.getElementById("content-page").style.display = "none";
+
+	if(recipes.length <= 0) {
+		document.getElementById("search-results").innerHTML = "<p style='font-size: 20px;'> No Recipes were found based on your search.</p>";
+		document.getElementById("search-list").innerHTML = "";
+
+	} else {
+		document.getElementById("search-results").innerHTML = "<p style='font-size: 20px;'>Found " + recipes.length + " matching recipes</p>";
+		var html = createResultsHTML(recipes);
+		document.getElementById("search-list").innerHTML = html;
+	}
+}
+
+function createResultsHTML(recipes) {
+	var html = "<ul class='none'>";
+
+	for(var i=0; i<recipes.length; i++) {
+		var recipee = recipes[i];
+
+		html +=
+			"<li><a class='menu' href=" + "/assets/recipes/" + recipe.url + "><span style='font-size: 18px;'>" + recipe.title + "</span></a></li>"
+	}
+
+	html += "</ul>";
+
+	return html;
+}
+
+var searchText = document.getElementById("searchText");
+
+searchText.addEventListener("keyup", function(event) {
+	if(event.keyCode === 13) {
+		event.preventDefault();
+		document.getElementById("searchBtn").click();
+	}
+});
+
 function menuFunction() {
   var x = document.getElementById("recipeTopnav");
   if (x.className === "topnav") {
