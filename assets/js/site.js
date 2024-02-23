55797bbb089f433a683825cdae6a11f3d69225df
ZajdelRecipes <160426132+ZajdelRecipes@users.noreply.github.com>
2024-02-23 13:53:19 -0500

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,9 +1,3 @@
-var recipeList = 
-[
-{"title":"Stuffed Peppers","url":"/stuffed-peppers.html"},
-{"title":"Baked Ziti","url":"/baked-ziti.html"}
-]
-
 function menuFunction() {
   var x = document.getElementById("recipeTopnav");
   if (x.className === "topnav") {
