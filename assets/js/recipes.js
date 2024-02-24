d0f1a43a931e46995a6e87b0c1f4c1124fdce29a
ZajdelRecipes <160426132+ZajdelRecipes@users.noreply.github.com>
2024-02-24 15:49:13 -0500

    Update recipes.js

--- assets/js/recipes.js
+++ assets/js/recipes.js
@@ -3,5 +3,6 @@ var recipeList =
 {"title":"Stuffed Peppers","url":"/stuffed-peppers.html","keywords":"diane"},
 {"title":"Baked Ziti","url":"/baked-ziti.html","keywords":"diane italian pasta"},
 {"title":"Stuffed Chicken Breasts with Mushroom Sauce","url":"/stuffed-chicken-breasts-with-mushroom-sauce.html","keywords":"diane"},
-{"title":"Olive Garden Chicken Gnochi Soup","url":"/olive-garden-chicken-gnochi-soup.html","keywords":"diane"}
+{"title":"Olive Garden Chicken Gnochi Soup","url":"/olive-garden-chicken-gnochi-soup.html","keywords":"diane"},
+{"title":"Chicken Noodle Soup","url":"/chicken-noodle-soup.html","keywords":"diane"}
 ]
